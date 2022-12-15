77a026e48b8e093f03d19abb3219035d4991d3f9
oe-tt <119619507+oe-tt@users.noreply.github.com>
2022-12-15 22:34:36 +0300

    Delete 001_Kvadrat_chisla directory

--- 001_Kvadrat_chisla/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿Console.Clear();
-Console.WriteLine ("Введите число");
-int a = int.Parse(Console.ReadLine());
-// int c = Convert.ToInt32 (Console.ReadLine());
-
-int b = a*a;
-Console.Write ("Квадрат числа: " + b);
-// Console.WriteLine (b);
