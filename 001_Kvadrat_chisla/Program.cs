d2b3d9c1e8da0b60fdecb9d818c6fd75269d1383
oe-tt <oe-tt@mail.ru>
2022-12-15 22:24:49 +0300

    001_Kvadrat_chisla

---
+++ 001_Kvadrat_chisla/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Clear();
+Console.WriteLine ("Введите число");
+int a = int.Parse(Console.ReadLine());
+// int c = Convert.ToInt32 (Console.ReadLine());
+
+int b = a*a;
+Console.Write ("Квадрат числа: " + b);
+// Console.WriteLine (b);
