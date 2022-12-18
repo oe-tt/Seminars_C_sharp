d60be43c5e1fe3daffb33639217cd4e497a39e0f
oe-tt <oe-tt@mail.ru>
2022-12-18 23:02:08 +0300

    Sravni 3 chisla

--- 01_Kvadrat_chisla/Program.cs
+++ 01_Kvadrat_chisla/Program.cs
@@ -1,8 +1,8 @@
 ﻿Console.Clear();
-Console.WriteLine ("Введите число");
+Console.WriteLine("Введите число");
 int a = int.Parse(Console.ReadLine());
 // int c = Convert.ToInt32 (Console.ReadLine());
 
-int b = a*a;
-Console.Write ("Квадрат числа: " + b);
+int b = a * a;
+Console.Write("Квадрат числа: " + b);
 // Console.WriteLine (b);
