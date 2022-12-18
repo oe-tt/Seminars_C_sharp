0bd5822f43467a830a76640e2c96394d3a78868a
oe-tt <oe-tt@mail.ru>
2022-12-18 23:55:10 +0300

    05_Vse_chetnye_chisla

---
+++ 05_Vse_chetnye_chisla/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Введите число");
+int a = int.Parse(Console.ReadLine());
+int b = 2;
+if (a < 2)
+{
+    Console.WriteLine("Введенное число слишком мало!");
+}
+while (b <= a)
+{
+    Console.Write(b);
+
+    if ((b < a) & (b + 1 < a))
+    {
+        Console.Write(", ");
+    }
+    else
+    {
+        Console.Write(".");
+    }
+    b += 2;
+}
