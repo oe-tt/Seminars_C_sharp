90c8cbe792c377d3c597074811745e0298a72ad3
oe-tt <oe-tt@mail.ru>
2022-12-18 22:58:14 +0300

    Sravni 3 chisla

---
+++ 03_Sravni_3_chisla/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("ведите три числа!");
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+int c = int.Parse(Console.ReadLine());
+int max = a;
+if (max < b)
+{
+    max = b;
+}
+if (max < c)
+{
+    max = c;
+}
+
+Console.Write("max = ");
+Console.WriteLine(max);
