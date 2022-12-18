915801627dcffa0c20425cc4bc7443dd30aafe76
oe-tt <oe-tt@mail.ru>
2022-12-18 23:34:18 +0300

    Chetnoe chislo

---
+++ 04_Chetnoe/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число");
+int a = int.Parse(Console.ReadLine());
+if (a%2 == 0)
+{
+    Console.WriteLine ("Да, число четное");
+}
+else
+{
+    Console.WriteLine ("Нет, число нечетное");
+}
