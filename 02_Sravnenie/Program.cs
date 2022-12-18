928ff87f6315196c3af22df0d9d2843486659827
oe-tt <oe-tt@mail.ru>
2022-12-18 22:08:47 +0300

    02_Sravnenie chisel

---
+++ 02_Sravnenie/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("ведите два числа!");
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+if (a > b)
+{
+    Console.Write ("max = ");
+    Console.WriteLine (a);
+}
+else
+{
+    if (a < b)
+    {
+        Console.Write ("max = ");
+        Console.WriteLine (b);
+    }
+    else
+    {
+        Console.WriteLine ("Числы равны!");
+    }
+}
